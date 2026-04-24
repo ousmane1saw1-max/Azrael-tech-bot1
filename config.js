db40b6a6cb05924e1cd6eb0048995faa99778873
ousmane1saw1-max <ousmane1saw1@gmail.com>
2026-04-24 15:23:44 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Number: "22666986425" // remplacez par votre numéro WhatsApp sans mettre le signe +
+  Number: "22650209596" // remplacez par votre numéro WhatsApp sans mettre le signe +
 };
