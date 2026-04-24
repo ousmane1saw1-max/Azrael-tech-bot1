a61df5eea77a5505020a24ad161855f02190cae9
ShadowTT1 <Sawadogoamad520@gmail.com>
2026-04-24 13:28:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import connectToWhatsapp from './Digix/crew.js'
 import handleIncomingMessage from './events/messageHandler.js'
-import config from './config.js' // On importe ta nouvelle configuration
 
 (async() => {
-    // On passe le numéro configuré à la fonction de connexion
-        await connectToWhatsapp(handleIncomingMessage, config.Number)
-            console.log('established !')
-            })()
+    await connectToWhatsapp(handleIncomingMessage)
+        console.log('established !')
+})()
